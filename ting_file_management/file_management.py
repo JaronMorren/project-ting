d567af3d584a74cae3e7fd65cdb8311a176b66f7
Jaron Morren <jaronmorren@gmail.com>
2023-06-29 22:06:36 -0300

    feat:Implemented txt importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # Checks if the file does not have a ".txt" extension
+    if not path_file.endswith(".txt"):
+        # Prints an error message to the standard error output using sys.stderr
+        return print("Formato inválido", file=sys.stderr)
+    try:
+        # Opens the file in read mode using a context manager
+        with open(path_file, "r") as file:
+            # Reads the contents of the file and splitting it into lines
+            lines = file.read().split("\n")
+        return lines
+    except FileNotFoundError:
+        # Prints an error message with the file name to sys.stderr
+        # if the file is not found
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
