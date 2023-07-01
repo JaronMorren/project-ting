7909e62e113cc5dd33427735c01b6023a53d9f77
Jaron Morren <jaronmorren@gmail.com>
2023-06-30 21:46:11 -0300

    feat:Implemented search by word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -34,4 +34,37 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    # Creates an empty list to store the results
+    results = []
+    # Iterates over each index in the instance
+    for index in range(len(instance)):
+        # Retrieves the file at the current index from the instance
+        file = instance.search(index)
+        # Creates a dictionary 'data' to store information
+        #  about the word occurrences in the file
+        data = {
+            "palavra": word,  # Stores the word being searched
+            "arquivo": file["nome_do_arquivo"],  # Stores the file name
+            "ocorrencias": [],
+            # Creates an empty list to store the occurrences
+        }
+        # Iterates over each line in the file
+        for line in file["linhas_do_arquivo"]:
+            # Checks if the word (case-insensitive) exists in the current line
+            if word.lower() in line.lower():
+                # If the word is found, appends the line number
+                #  and content to the 'ocorrencias' list
+                data["ocorrencias"].append(
+                    {
+                        "linha": file["linhas_do_arquivo"].index(line) + 1,
+                        "conteudo": line,
+                    }
+                )
+        # Checks if there are any occurrences for the current file
+        if data["ocorrencias"]:
+            # If there are occurrences,
+            # appends the 'data' dictionary to the 'results' list
+            results.append(data)
+        # Returns the 'results' list containing
+        #  the occurrences of the word in the files
+        return results
